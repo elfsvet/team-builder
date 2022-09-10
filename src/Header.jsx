7c9f536ff4a3d349a40bc308eeb4a9aa9048c154
elfsvet <stepanmatysik@gmail.com>
2022-09-10 13:36:54 -0400

    need to add context

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const Header = () => {
+const Header = ({ selectedTeam, teamMemberCount }) => {
   return (
-    <h1>Header</h1>
-  )
-}
+    <header className='container'>
+      <div className='row justify-content-center mt-3 mb-4'>
+        <div className='col-8'>
+          <h1>Team Member Allocation</h1>
+          <h3>
+            {selectedTeam} has {teamMemberCount} {teamMemberCount === 1 ? 'Member' : "Members"}
+          </h3>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/TeamMembers.jsx
@@ -0,0 +1,10 @@
+import TeamMemberCard from "./TeamMemberCard";
+const TeamMembers = ({employees, handleEmployeeCardClick, selectedTeam}) => {
+return (
+    employees.map((employee) => (
+        <TeamMemberCard key={employee.id} handleEmployeeCardClick={handleEmployeeCardClick} selectedTeam={selectedTeam} employee={employee}/>
+    ))
+)
+}
+
+export default TeamMembers;
